c61df11a6a7b283079c71e4586a2f696a6fe1e4d
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-02-25 16:08:23 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
 					<div class="col-sm-12 text-center">
 						<p class="copyright">Copyright &copy <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | <a<?php if (!is_front_page()) { ?> rel="nofollow"<?php } ?> target="_blank" href="https://www.bluesummitcreative.com/">Website Design by Blue Summit Creative</a> | 
 						
-						<a href="https://www.facebook.com/TrueYouInMotion/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+						<a target="_blank" href="https://www.facebook.com/TrueYouInMotion/"><i class="fab fa-facebook-square"></i></a>
 							
-						<a href="https://www.instagram.com/trueyouinmotion/" target="_blank"><i class="fab fa-instagram"></i></a>
+						<a target="_blank" href="https://www.instagram.com/trueyouinmotion/"><i class="fab fa-instagram"></i></a>
 							
 						</p>
 					</div>
