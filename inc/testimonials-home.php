d2790a75aa140472d8a769a912a668096cb15429
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-02-25 15:19:27 -0700

    Update testimonials-home.php

--- inc/testimonials-home.php
+++ inc/testimonials-home.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="container-fluid home-testimonials">
 	<div class="container">
 		<div class="row">
 
@@ -14,13 +14,12 @@
 		if ( $query_testimonials->have_posts() ) {
 			while ( $query_testimonials->have_posts() ) {
 				$query_testimonials->the_post(); ?>
-			<div class="slide-item">
-				<div class="inner-box">
-					<div class="slide-header">
-						<figure class="author-thumb"><img src="<?php the_sub_field('image'); ?>" alt=""></figure>
-						<h4><?php the_title(); ?></h4>
+			<div class="col-sm-4">
+				<div class="testimonial">
+					PUT FEATURED IMAGE HERE
+					<h4><?php the_title(); ?></h4>
 					</div>
-					<div class="slide-content"><?php the_content(); ?></div>
+					<div class="testimonial-content"><?php the_content(); ?></div>
 				</div>
 			</div>
 
