a77230acd21a330e3b40e33ec40b0b91e4328978
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-02-25 16:31:25 -0700

    Update testimonials-home.php

--- inc/testimonials-home.php
+++ inc/testimonials-home.php
@@ -1,45 +1,38 @@
-<div class="container-fluid home-testimonials">
-	<div id="carouselExample" class="carousel slide container" data-ride="carousel" data-interval="3000">
-		<div class="carousel-inner row w-100 mx-auto" role="listbox">
-
-	<?php
-		// WP_Query arguments
-		$args = array(
-			'post_type'              => array( 'testimonials' ),
-			'post_status'            => array( 'publish' ),
-		);
-
-		$query_testimonials = new WP_Query( $args );
-		
-		$c = 0;
-					
-		
-
-		if ( $query_testimonials->have_posts() ) {
-			while ( $query_testimonials->have_posts() ) {
-				$query_testimonials->the_post(); 
-				$c++;
-				if ( $c == 1 ){ $active_class = ' active';}
-				else{ $active_class = '';} 
-			?>
-			<div class="carousel-item col-sm-4<?php echo $active_class; ?>">
-				<div class="testimonial">
-					<img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=<?php echo $c; ?>" alt="slide <?php echo $c; ?>">
-					<h3><?php the_title(); ?></h3>
-					<div class="testimonial-content"><?php the_content(); ?></div>
-				</div>
-			</div>
-
-		<?php }} else {} wp_reset_postdata(); ?>
-		
-		</div>
-		<a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-		    <i class="fa fa-chevron-left fa-lg text-muted"></i>
-		    <span class="sr-only">Previous</span>
-		</a>
-		<a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
-		    <i class="fa fa-chevron-right fa-lg text-muted"></i>
-		    <span class="sr-only">Next</span>
-		</a>
-	</div>
+<div class="container-fluid">
+    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
+        <div class="carousel-inner row w-100 mx-auto" role="listbox">
+            <div class="carousel-item col-md-3 active">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400/000/fff?text=1" alt="slide 1">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=2" alt="slide 2">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=3" alt="slide 3">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=4" alt="slide 4">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=5" alt="slide 5">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=6" alt="slide 6">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=7" alt="slide 7">
+            </div>
+            <div class="carousel-item col-md-3">
+                <img class="img-fluid mx-auto d-block" src="//placehold.it/600x400?text=8" alt="slide 7">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+            <i class="fa fa-chevron-left fa-lg text-muted"></i>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+            <i class="fa fa-chevron-right fa-lg text-muted"></i>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </div>
