17b3a35f94d2271e468a72625e2d0f1f3ad91ba6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-02-25 15:27:26 -0700

    Update testimonials-home.php

--- inc/testimonials-home.php
+++ inc/testimonials-home.php
@@ -1,6 +1,6 @@
 <div class="container-fluid home-testimonials">
-	<div class="container">
-		<div class="row">
+	<div id="carouselExample" class="carousel slide container" data-ride="carousel" data-interval="9000">
+		<div class="carousel-inner row w-100 mx-auto" role="listbox">
 
 	<?php
 		// WP_Query arguments
@@ -14,7 +14,7 @@
 		if ( $query_testimonials->have_posts() ) {
 			while ( $query_testimonials->have_posts() ) {
 				$query_testimonials->the_post(); ?>
-			<div class="col-sm-4">
+			<div class="carousel-item col-sm-4 active">
 				<div class="testimonial">
 					PUT FEATURED IMAGE HERE
 					<h3><?php the_title(); ?></h3>
