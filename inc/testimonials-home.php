2a6490e9277e097be2c67d203287a1ebb31773fd
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-02-25 17:03:29 -0700

    Update testimonials-home.php

--- inc/testimonials-home.php
+++ inc/testimonials-home.php
@@ -24,7 +24,7 @@
 			?>
 			<div class="carousel-item col-sm-3<?php echo $active_class; ?>">
 				<div class="testimonial text-center">					
-						<img style="border-radius:50%;" class="img-fluid mx-auto d-block" src="//placehold.it/400x400?text=<?php echo $c; ?>" alt="slide <?php echo $c; ?>">
+						<img style="border-radius:50%;margin:auto;" class="mx-auto d-block" src="//placehold.it/200x200?text=<?php echo $c; ?>" alt="slide <?php echo $c; ?>">
 						<h3><?php the_title(); ?></h3>
 						<?php the_content(); ?>
 				</div>
