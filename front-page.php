c156f41cafff97e33d21cfced2c6cc13c286c224
Jake Fedie <jake@graffedie.com>
2018-02-27 12:45:50 -0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
 	get_template_part('inc/slider-home');
 	get_template_part('inc/intro-home');
   	get_template_part('inc/section-banner');
-        get_template_part('inc/wide-image');
+        get_template_part('inc/section-banner');
 	get_template_part('inc/funnels-title');
 	get_template_part('inc/funnels-home');
 	get_template_part('inc/testimonials-home');
