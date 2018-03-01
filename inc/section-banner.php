91941927b375a5df24a035b3bb5bf1fb149b4ae7
Jake Fedie <jake@graffedie.com>
2018-02-28 18:21:48 -0700

    Update section-banner.php

--- inc/section-banner.php
+++ inc/section-banner.php
@@ -1,4 +1,14 @@
 
-<?php img src="<?php the_field('section_banner'); ?>" class="img-fluid">
+<?php 
 
+$image = get_field('section_banner');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+
+if( $image ) {
+
+	echo wp_get_attachment_image( $image, $size );
+
+}
+
+?>
 
