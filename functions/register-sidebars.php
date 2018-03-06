49b59e57f3c3058a06e789e428d7a87dea96fc62
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-06 13:05:26 -0700

    Update register-sidebars.php

--- functions/register-sidebars.php
+++ functions/register-sidebars.php
@@ -24,6 +24,50 @@
 				)
 		);
 		
+		register_sidebar( 
+			array(
+				'name' => __( 'Footer Widget 1'),
+				'id' => 'footer-widget-1',
+				'before_widget' => '<div class="widget">',
+				'after_widget' => '</div>',
+				'before_title' => '<h4>',
+				'after_title' => '</h4>',
+				)
+		);
+		
+		register_sidebar( 
+			array(
+				'name' => __( 'Footer Widget 2'),
+				'id' => 'footer-widget-2',
+				'before_widget' => '<div class="widget">',
+				'after_widget' => '</div>',
+				'before_title' => '<h4>',
+				'after_title' => '</h4>',
+				)
+		);
+		
+		register_sidebar( 
+			array(
+				'name' => __( 'Footer Widget 3'),
+				'id' => 'footer-widget-3',
+				'before_widget' => '<div class="widget">',
+				'after_widget' => '</div>',
+				'before_title' => '<h4>',
+				'after_title' => '</h4>',
+				)
+		);
+		
+		register_sidebar( 
+			array(
+				'name' => __( 'Footer Widget 4'),
+				'id' => 'footer-widget-4',
+				'before_widget' => '<div class="widget">',
+				'after_widget' => '</div>',
+				'before_title' => '<h4>',
+				'after_title' => '</h4>',
+				)
+		);
+		
 	}
 	add_action( 'widgets_init', 'custom_sidebars' );
 ?>
