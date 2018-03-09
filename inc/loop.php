718973f7e3dca6310d312d0fbca3af571e32883b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-09 15:12:56 -0700

    Update loop.php

--- inc/loop.php
+++ inc/loop.php
@@ -1,5 +1,7 @@
 <?php if ( is_page_template('full-width.php') ) { ?>
 <div class="col-sm-12">
+<?php } elseif ( is_single() ) { ?>
+<div class="col-sm-12">
 <?php } else { ?>
 <div class="col-sm-9 order-last">
 <?php } ?>
